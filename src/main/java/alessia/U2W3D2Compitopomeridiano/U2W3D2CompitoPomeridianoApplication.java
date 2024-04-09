40f1b39c2a7d4156dd9eed2076f7b1145a0dae5e
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-04-09 14:36:49 +0200

    initial commit

---
+++ src/main/java/alessia/U2W3D2Compitopomeridiano/U2W3D2CompitoPomeridianoApplication.java
@@ -0,0 +1,13 @@
+package alessia.U2W3D2Compitopomeridiano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W3D2CompitoPomeridianoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W3D2CompitoPomeridianoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alessia/U2W3D2Compitopomeridiano/U2W3D2CompitoPomeridianoApplicationTests.java
@@ -0,0 +1,13 @@
+package alessia.U2W3D2Compitopomeridiano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W3D2CompitoPomeridianoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
